7ed7b9c1fad5b0865cad206e50ae29a751ec4f2d
StarHack <danielschmidth@googlemail.com>
2025-07-28 05:12:33 +0200

    implement MoveFile

--- files/files.go
+++ files/files.go
@@ -100,3 +100,40 @@ func UpdateFileMeta(cfg *config.Config, fileUUID string, updated *folders.File) 
 
 	return &updatedFile, nil
 }
+
+// MoveFile moves the file with the given UUID to the destination folder.
+func MoveFile(cfg *config.Config, fileUUID, destinationFolderUUID string) (*folders.File, error) {
+	endpoint := cfg.DriveAPIURL + filesPath + "/" + fileUUID
+
+	body, err := json.Marshal(map[string]string{
+		"destinationFolder": destinationFolderUUID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPatch, endpoint, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+cfg.Token)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		respBody, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("MoveFile failed: %d %s", resp.StatusCode, string(respBody))
+	}
+
+	var movedFile folders.File
+	if err := json.NewDecoder(resp.Body).Decode(&movedFile); err != nil {
+		return nil, err
+	}
+
+	return &movedFile, nil
+}
