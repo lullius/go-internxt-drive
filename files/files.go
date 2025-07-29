991258f28d597e90dce2242116133ec0e46c6490
StarHack <danielschmidth@googlemail.com>
2025-07-29 18:28:06 +0200

    add rename file function

--- files/files.go
+++ files/files.go
@@ -137,3 +137,40 @@ func MoveFile(cfg *config.Config, fileUUID, destinationFolderUUID string) (*fold
 
 	return &movedFile, nil
 }
+
+// RenameFile renames a file by UUID with the given new name and optional type.
+func RenameFile(cfg *config.Config, fileUUID, newPlainName, newType string) error {
+	endpoint := cfg.DriveAPIURL + filesPath + "/" + fileUUID + "/meta"
+
+	payload := map[string]string{
+		"plainName": newPlainName,
+	}
+	if newType != "" {
+		payload["type"] = newType
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPut, endpoint, bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+cfg.Token)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		respBody, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("RenameFile failed: %d %s", resp.StatusCode, string(respBody))
+	}
+
+	return nil
+}
